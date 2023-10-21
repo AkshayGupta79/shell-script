35ae785baf4c9e11aa849f7d5d5e11b9ec931c69
AkshayGupta79 <akshay.gupta799@gmail.com>
2023-10-21 06:48:36 +0000

    shell script digital watch project

--- digital_clock.sh
+++ digital_clock.sh
@@ -3,7 +3,7 @@
 #to keep running this in loop we are using while
 
 #if you want your watch in different colors
-#you canuse any of color
+#you can use so that we can use these
 Red=$'\e[1;31m'
 Grren=$'\e[1;32m'
 Blue=$'\e[1;34m'
