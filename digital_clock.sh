6389b001e4ca3515591f674e6dc769fd4a098c4e
akshay <akshay.gupta799@gmail.com>
2025-08-11 16:35:38 +0530

    digital clock script

--- digital_clock.sh
+++ digital_clock.sh
@@ -2,6 +2,7 @@
 #clear ; date +%T ; sleep 1 ; clear ; date +%T ; sleep 1
 #to keep running this in loop we are using while
 
+
 #if you want your watch in different colors
 #you can use so that we can use these
 Red=$'\e[1;31m'
